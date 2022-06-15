b9612640d08d891033306986893ded4613c28c87
Vika Tsykunova <vika.tsykunova02@gmail.com>
2022-06-15 20:10:05 +0300

    Lover Flower

--- thirdpage/script.js
+++ thirdpage/script.js
@@ -1,12 +1,3 @@
-let range = document.querySelector('.number');
-let rangeNum = document.querySelector('.range-num');
-range.oninput = function () {
-    console.log(this.value);
-    rangeNum.style.left = this.value + 'px';
-    rangeNum.innerHTML = this.value;
-};
-
-
 function burgerMenu(selector) {
 	let menu = $(selector);
 	let button = menu.find('.burger-menu__button');
